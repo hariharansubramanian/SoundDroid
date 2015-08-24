3852d0ffed898743fd7a1bd0819ab75058d14dd1
hariharansubramanian <hariharan1990.s@gmail.com>
2015-08-25 00:57:39 +0530

    Created class Track members: title, streamURL, id Getters and Setters

---
+++ app/src/main/java/com/example/hari/sounddroid/Track.java
@@ -0,0 +1,35 @@
+package com.example.hari.sounddroid;
+
+/**
+ * Created by Nicky on 8/24/2015.
+ */
+public class Track {
+    private String title;
+    private String streamURL;
+    private int id;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStreamURL() {
+        return streamURL;
+    }
+
+    public void setStreamURL(String streamURL) {
+        this.streamURL = streamURL;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
+
