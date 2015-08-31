1f58ce0ac5bd21d71f21f4c3fa05ae67df1ddd5b
hariharansubramanian <hariharan1990.s@gmail.com>
2015-08-31 14:42:28 +0530

    Deleted Gson implementation

--- app/src/main/java/com/example/hari/sounddroid/MainActivity.java
+++ app/src/main/java/com/example/hari/sounddroid/MainActivity.java
@@ -2,11 +2,9 @@ package com.example.hari.sounddroid;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.google.gson.Gson;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,19 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Gson gson = new Gson();
-        Track track=gson.fromJson(trackJSON(),Track.class);
-        /*automatically maps Json string members to Track members
-        Can alternatively be done by using @SerializedName("title"),@SerializedName("stream_url"),
-        @SerializedName("id")*/
-        Log.d("Gson", "Track title is : " + track.getTitle());
-
-
-    }
-
-    private String trackJSON() {
-        return "{\"kind\":\"track\",\"id\":13158665,\"created_at\":\"2011/04/06 15:37:43 +0000\",\"user_id\":3699101,\"duration\":18109,\"commentable\":true,\"state\":\"finished\",\"original_content_size\":201483,\"last_modified\":\"2013/02/18 19:18:11 +0000\",\"sharing\":\"public\",\"tag_list\":\"soundcloud:source=iphone-record\",\"permalink\":\"munching-at-tiannas-house\",\"streamable\":true,\"embeddable_by\":\"all\",\"downloadable\":false,\"purchase_url\":null,\"label_id\":null,\"purchase_title\":null,\"genre\":null,\"title\":\"Munching at Tiannas house\",\"description\":null,\"label_name\":null,\"release\":null,\"track_type\":\"recording\",\"key_signature\":null,\"isrc\":null,\"video_url\":null,\"bpm\":null,\"release_year\":null,\"release_month\":null,\"release_day\":null,\"original_format\":\"m4a\",\"license\":\"all-rights-reserved\",\"uri\":\"https://api.soundcloud.com/tracks/13158665\",\"user\":{\"id\":3699101,\"kind\":\"user\",\"permalink\":\"alex-stevenson\",\"username\":\"Alex Stevenson\",\"last_modified\":\"2011/06/13 23:58:44 +0000\",\"uri\":\"https://api.soundcloud.com/users/3699101\",\"permalink_url\":\"http://soundcloud.com/alex-stevenson\",\"avatar_url\":\"https://i1.sndcdn.com/avatars-000004193858-jnf2pd-large.jpg\"},\"created_with\":{\"id\":124,\"kind\":\"app\",\"name\":\"SoundCloud iOS\",\"uri\":\"https://api.soundcloud.com/apps/124\",\"permalink_url\":\"http://soundcloud.com/apps/iphone\",\"external_url\":\"http://itunes.com/app/soundcloud\"},\"permalink_url\":\"http://soundcloud.com/alex-stevenson/munching-at-tiannas-house\",\"artwork_url\":null,\"waveform_url\":\"https://w1.sndcdn.com/fxguEjG4ax6B_m.png\",\"stream_url\":\"https://api.soundcloud.com/tracks/13158665/stream\",\"playback_count\":6319,\"download_count\":134,\"favoritings_count\":2,\"comment_count\":3,\"attachments_uri\":\"https://api.soundcloud.com/tracks/13158665/attachments\",\"policy\":\"ALLOW\",\"monetization_model\":\"NOT_APPLICABLE\"}";
 
     }
 
---
+++ app/src/main/java/com/example/hari/sounddroid/SoundCloudService.java
@@ -0,0 +1,8 @@
+package com.example.hari.sounddroid;
+
+/**
+ * Created by Nicky on 8/31/2015.
+ */
+public interface SoundCloudService {
+    
+}
