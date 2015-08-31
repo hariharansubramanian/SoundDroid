a399c8376a70ab703138d217e3e1c66b838c1317
hariharansubramanian <hariharan1990.s@gmail.com>
2015-08-31 14:57:47 +0530

    Added an interface SoundCLoudService @GET(/tracks?client_id=CLIENT_ID) public List<Track> searchSongs(@Query(q) String query) which enables you to search for a particular song as query

--- app/src/main/java/com/example/hari/sounddroid/SoundCloudService.java
+++ app/src/main/java/com/example/hari/sounddroid/SoundCloudService.java
@@ -1,8 +1,18 @@
 package com.example.hari.sounddroid;
 
+import java.util.List;
+
+import retrofit.http.GET;
+import retrofit.http.Query;
+
 /**
  * Created by Nicky on 8/31/2015.
  */
 public interface SoundCloudService {
-    
+    //@GET can only have constants hence we declare as constant
+    static final String CLIENT_ID="57ffe342c618cb46f9485d712bb66cbf";
+
+    @GET("/tracks?client_id="+CLIENT_ID)
+    public List<Track> searchSongs(@Query("q") String query);
+
 }
